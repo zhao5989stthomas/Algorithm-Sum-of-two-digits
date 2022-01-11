5047260836ab75becb6e651a548a4fdf3064cfc4
zhao5989stthomas <78622211+zhao5989stthomas@users.noreply.github.com>
2022-01-11 01:37:58 -0600

    Update APlusB_2.py

--- APlusB_2.py
+++ APlusB_2.py
@@ -6,7 +6,7 @@ def sum_of_two_digits(first_digit, second_digit):
     return sum    
 
 if __name__ == '__main__':
-    input = input("Please enter two digits separated by space: ").split()
-    a = int(input[0])
-    b = int(input[1])
+    inp = input("Please enter two digits separated by space: ").split()
+    a = int(inp[0])
+    b = int(inp[1])
     print (sum_of_two_digits(a, b))
