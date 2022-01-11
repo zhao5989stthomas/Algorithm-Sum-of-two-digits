c8f86ef2b913403295be172155c1280f501fbf02
zhao5989stthomas <78622211+zhao5989stthomas@users.noreply.github.com>
2022-01-11 01:41:18 -0600

    map(int, input().split())

--- APlusB.py
+++ APlusB.py
@@ -6,3 +6,9 @@ def sum_of_two_digits(first_digit, second_digit):
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(sum_of_two_digits(a, b))
+
+# a, b = map(int, input().split())
+# input() will query the user for input, and read one line of user input;
+# split() will split that input into a list of "words";
+# map(int, ...) will call int on each word, it will to that lazily (although that is not important here); and
+# a, b = ... will unpack the expression into two elements, and assign the first one to n and the second one to S.
